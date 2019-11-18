8cbcc0bd42df6790394ff0560068fb8cddc945de
merkax <mishabigr@yandex.ru>
2019-11-19 02:22:30 +0300

    Add class method for Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,6 @@
 class Test < ApplicationRecord
-
-# Создайте метод класса в модели Test, который возвращает отсортированный по убыванию массив названий всех Тестов
-# у которых Категория называется определённым образом (название категории передается в метод в качестве аргумента). 
-  def self.sort_categories(categories)
-    tests.where(title: :categories).order(id: :desc)
+  def self.sort_categories(category)
+    Test.joins('JOIN categories ON tests.category_id = categories.id')
+    .where(categories: { title: :category }).order(title: :desc).pluck(:title)
   end
 end
