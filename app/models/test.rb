398e920df1003ab22dccd73696eb838a4716ba63
merkax <mishabigr@yandex.ru>
2019-11-19 01:56:15 +0300

    Add test_by_level method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+
+# Создайте метод класса в модели Test, который возвращает отсортированный по убыванию массив названий всех Тестов
+# у которых Категория называется определённым образом (название категории передается в метод в качестве аргумента). 
+  def self.sort_categories(categories)
+    tests.where(title: :categories).order(id: :desc)
+  end
 end
