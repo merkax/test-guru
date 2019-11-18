5da1bb79bcdb80fe51d1a9cb43743c6bf892df98
merkax <mishabigr@yandex.ru>
2019-11-18 15:57:56 +0300

    Add null constraints

---
+++ db/migrate/20191118123046_add_null_constraints.rb
@@ -0,0 +1,11 @@
+class AddNullConstraints < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null(:tests, :title, false)
+    change_column_null(:tests, :level, false)
+    change_column_null(:users, :name, false)
+    change_column_null(:questions, :body, false)
+    change_column_null(:categories, :title, false)
+    change_column_null(:answers, :body, false)
+    change_column_null(:answers, :correct, false)
+  end
+end
