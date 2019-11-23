c94b96372356bd7fa01f012fb7710f34b336cd43
merkax <mishabigr@yandex.ru>
2019-11-23 23:27:43 +0300

    Fix homework6

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,15 +7,15 @@ class Test < ApplicationRecord
 
   validates :title, presence: true,
             uniqueness: { scope: :level }
-  validates :level, numericality: { only_integer: true, :greater_than_or_equal_to 0 }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   scope :easy, -> { where(level: 0..1) }
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
   scope :sort_by_categories, -> (category) {
-    joins(:category).where(categories: { title: category }).order(title: :desc) }
+    joins(:category).where(categories: { title: category }) }
 
   def self.sort_categories(category)
-    sort_by_categories(category).pluck(:title)
+    sort_by_categories(category).order(title: :desc).pluck(:title)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_23_172214) do
+ActiveRecord::Schema.define(version: 2019_11_23_192859) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2019_11_23_172214) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "email"
+    t.string "email", null: false
   end
 
   add_foreign_key "answers", "questions"
