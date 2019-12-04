acb8598d0b646d8f4d2c726400498ae6935bc6a0
merkax <mishabigr@yandex.ru>
2019-12-04 15:39:20 +0300

    Add first scrincast from lesson10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+  
+  include Auth
+
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id, dependent: :nullify
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
-  validates :name,:email, presence: true
-
+  has_secure_password
+  
   scope :test_by_level, -> (level) { tests.where(level: level) }
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
-  
 end
