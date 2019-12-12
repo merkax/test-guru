ad4333db492fb9467cf5626ca18b6860e3aa7214
merkax <mishabigr@yandex.ru>
2019-12-12 23:39:46 +0300

    fix for homework12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,13 +9,11 @@ class ApplicationController < ActionController::Base
     { lang: I18n.locale == I18n.default_locale ? nil : I18n.locale}
   end
 
-
   protected
 
   def set_locale
     I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
-  
 
   def after_sign_in_path_for(current_user)
     current_user.admin? ? admin_tests_path : root_path
