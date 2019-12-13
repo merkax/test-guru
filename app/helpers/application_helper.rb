e28086dad097ffeffa3ec2cff0671df178bca6fb
merkax <mishabigr@yandex.ru>
2019-12-13 20:39:11 +0300

    add homework13

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,11 @@ module ApplicationHelper
     link_to 'Github', "https://github.com/#{author}/#{repo}", target:'_blank'
   end
 
-  # def flash_message(type)
-  #   content_tag :p, flash[type], class: "flash #{type}" if flash[type]
-  # end
+  def flash_message(type)
+    {
+      notice: 'alert-info',
+      alert: 'alert-danger'
+    }[type.to_sym]
+    #content_tag :p, flash[type], class: "flash #{type}" if flash[type]
+  end
 end
