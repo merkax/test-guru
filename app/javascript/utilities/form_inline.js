6e3de78f35340956e71dfb8531ac313f960b2b46
merkax <mishabigr@yandex.ru>
2020-02-11 21:54:19 +0300

    add locales for new buttons

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -27,13 +27,16 @@ function formInlineHandler(testId) {
   var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
   var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
 
+  var newButton = link.dataset.newButton
+  var oldButton = link.dataset.oldButton
+
   if (formInline.classList.contains('hide')){
     testTitle.classList.add('hide')
     formInline.classList.remove('hide')
-    link.textContent = 'Cancel'
+    link.textContent = newButton
   } else {
     testTitle.classList.remove('hide')
     formInline.classList.add('hide')
-    link.textContent = 'Edit'
+    link.textContent = oldButton
   }
  }
