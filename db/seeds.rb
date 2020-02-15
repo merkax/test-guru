f7e2c80f56f6c7f1784b0049b43069fc505be903
merkax <mishabigr@yandex.ru>
2020-02-15 13:17:20 +0300

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-  users = User.create!(name: 'Mike', name: 'Ivan')
+  users = User.create!([
+                        {email: 'mishagrishkinandco@gmail.com',
+                        password: 'secret1234', 
+                        password_confirmation: 'secret1234',
+                        first_name: 'Mike', 
+                        last_name: 'admin', 
+                        type: 'Admin'}
+                      ])
 
   categories  = Category.create!([
-    { title: 'Frontend' },
-    { title: 'Backend' }
-  ])
+                                  { title: 'Frontend' },
+                                  { title: 'Backend' }
+                                ])
 
   tests = Test.create!([
-    { title: 'Ruby', level: 2, category_id: categories[1].id },
-    { title: 'HTML', level: 1, category_id: categories[0].id },
-    { title: 'JavaScript', level: 2, category_id: categories[0].id }
-  ])
+                        { title: 'Ruby', level: 2, category_id: categories[1].id, author_id: users[0].id },
+                        { title: 'HTML', level: 1, category_id: categories[0].id, author_id: user[0].id },
+                        { title: 'JavaScript', level: 2, category_id: categories[0].id, author_id: user[0].id }
+                      ])
 
   questions = Question.create!([
-    { body: 'Who is Matz?', test_id: tests[0].id },
-    { body: 'Who is Berners-Lee?', test_id: tests[1].id },
-    { body: 'Who is Brendan Eich?', test_id: tests[2].id }
-  ])
+                                { body: 'Who is Matz?', test_id: tests[0].id },
+                                { body: 'Who is Berners-Lee?', test_id: tests[1].id },
+                                { body: 'Who is Brendan Eich?', test_id: tests[2].id }
+                              ])
 
   answers = Answer.create!([
-    {
-      body:'He is Ruby creator'
-      correct: true
-      question_id: questions[0].id
-    },
-    {
-      body:'He is HTML creator'
-      correct: false
-      question_id: questions[0].id
-    }
-  ])
+                            {
+                              body:'He is Ruby creator'
+                              correct: true
+                              question_id: questions[0].id
+                            },
+                            {
+                              body:'He is HTML creator'
+                              correct: false
+                              question_id: questions[0].id
+                            }
+                          ])
