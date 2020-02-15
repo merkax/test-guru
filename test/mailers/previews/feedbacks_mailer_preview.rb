983843a2ec7f91017174b9008ffa6ae6082e538a
merkax <mishabigr@yandex.ru>
2020-02-15 17:25:19 +0300

    add test previews for feedbacks mailer

--- test/mailers/previews/feedbacks_mailer_preview.rb
+++ test/mailers/previews/feedbacks_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/feedbacks_mailer
 class FeedbacksMailerPreview < ActionMailer::Preview
-
+  def feedback_message
+    feedback = Feedback.new(user: User.first, message: Feedback.first.message)
+    
+    FeedbacksMailer.feedback_message(feedback)
+  end
 end
