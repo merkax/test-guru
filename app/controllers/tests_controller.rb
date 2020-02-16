961f0109f865c18bbebabed8b01b7bb3e6fe10cb
merkax <mishabigr@yandex.ru>
2020-02-16 23:00:47 +0300

    fixed nilclass in the absence of a quest

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -11,7 +11,7 @@ class TestsController < ApplicationController
 
   def start
     current_user.tests.push(@test)
-    redirect_to current_user.test_passage(@test)
+    redirect_to current_user.test_passage(@test) unless current_user.test_passage(@test).completed?
   end
 
   private
